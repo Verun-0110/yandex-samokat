f3de05dba1313679742d271a2e40d6f8291c6cc0
AlexExiv <AlexExiv@mail.ru>
2023-11-05 18:27:25 +0500

    new projects orders yandex samokat mp

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://dae5c230-358f-4e41-a32c-ce58eb65812b.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://9233ff46-571e-45f2-ae37-86c9776b844b.serverhub.praktikum-services.ru"
 CREATE_ORDER= "/api/v1/orders"
 GET_ORDER_BY_TRACK = "/api/v1/orders/track"
